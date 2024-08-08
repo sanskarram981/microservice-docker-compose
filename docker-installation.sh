ed5841566318175234502a330836af0b2ae84478
Sanskar <sanskarram992@gmail.com>
2024-08-09 02:44:12 +0530

    changes done

---
+++ docker-installation.sh
@@ -0,0 +1,10 @@
+sudo apt update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+docker --version
+sudo systemctl status docker
+sudo usermod -aG docker $USER
+sudo apt install docker-compose
+docker run hello-world
